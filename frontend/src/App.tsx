6b9a0829ebfeff8ac2710a150e3bd59b6df7de14
Pablo Ibarra <pabloo_ibarra98@hotmail.com>
2025-01-27 01:28:09 -0600

    initial ui template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BotMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "./components";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Message {
+  text: string;
+  isBot: boolean;
+  info?: {
+    message: string;
+  };
 }
 
-export default App
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isBot: false }]);
+
+    // TODO: Call API
+    // const { ok, message } = await botService(text);
+    // if (!ok) {
+    //   setMessages((prev) => [
+    //     ...prev,
+    //     { text: "Hubo un error", isBot: true },
+    //   ]);
+    // } else {
+    //   setMessages((prev) => [
+    //     ...prev,
+    //     {
+    //       text: message,
+    //       info: {
+    //         message,
+    //       },
+    //       isBot: true,
+    //     },
+    //   ]);
+    // }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <main className="flex flex-row mt-7">
+      <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
+        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
+          Láak Chatbot
+        </h1>
+
+        <div className="border-gray-700 border" />
+      </nav>
+
+      <section className="mx-3 sm:mx-20 flex flex-col w-full h-[calc(100vh-50px)]  bg-white bg-opacity-10 p-5 rounded-3xl">
+        <div className="flex flex-row h-full">
+          <div className="flex flex-col flex-auto h-full p-1">
+            <div className="chat-container">
+              <div className="chat-messages">
+                <div className="grid grid-cols-12 gap-y-2">
+                  <BotMessage text="Hola, ¿en qué puedo ayudarte?" />
+
+                  {messages.map((message, index) =>
+                    message.isBot ? (
+                      <BotMessage key={index} text={message.info!.message} />
+                    ) : (
+                      <MyMessage key={index} text={message.text} />
+                    )
+                  )}
+
+                  {isLoading && (
+                    <div className="col-start-1 col-end-12 fade-in">
+                      <TypingLoader />
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <TextMessageBox
+                onSendMessage={handlePost}
+                placeholder="Escribe aquí lo que deseas"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './chat-bubbles/BotMessage';
+export * from './chat-bubbles/MyMessage';
+export * from './chat-input-boxes/TextMessageBox';
+export * from './loaders/TypingLoader';
